68676975342739e352d7fd9871e2d91612e2e53e
Pawel Barul <pawel.barul@programisci.eu>
2018-09-10 22:59:52 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ProfileCardComponent} from './components/profile-card/profile-card.component';
 import {UserInfoCardComponent} from './components/user-info-card/user-info-card.component';
+import {CommentsCardComponent} from './components/comments-card/comments-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileCardComponent,
-    UserInfoCardComponent
+    UserInfoCardComponent,
+    CommentsCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/comments-card/comments-card.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-comments-card',
+  templateUrl: './comments-card.component.html',
+  styleUrls: ['comments-card.component.scss']
+})
+
+export class CommentsCardComponent {
+
+}
